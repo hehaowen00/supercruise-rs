24e139eb674be6b506849592a246e3aedfdff065
hehaowen00 <edmundhe@outlook.com>
2022-06-24 01:18:08 +1000

    send back bad request response for invalid requests in websocket upgrader

--- src/ws.rs
+++ src/ws.rs
@@ -17,20 +17,39 @@ impl WsUpgrader {
 
     pub async fn upgrade(stream: &mut TcpStream, req: &Request<Body>) -> std::io::Result<()> {
         let headers = req.headers();
+        let mut buf = BytesMut::new();
+
         if !headers.contains_key(CONNECTION) || !headers.contains_key(SEC_WEBSOCKET_KEY) {
+            let resp: Response<Body> = Response::builder()
+                .status(401)
+                .header("Content-Type", "text/plain")
+                .body("400 Bad Request".into())
+                .unwrap();
+
+            let mut http: Http<_> = Http::new();
+            http.encode(resp, &mut buf).unwrap();
+            stream.write(&buf).await?;
+
             return Err(std::io::Error::other("upgrade failed"));
         }
 
-        let mut http: Http<()> = Http::new();
-        let mut builder = Response::builder();
-        builder = builder.header(UPGRADE, "websocket");
-        builder = builder.header(CONNECTION, "Upgrade");
+        let mut builder = Response::builder()
+            .header(UPGRADE, "websocket")
+            .header(CONNECTION, "Upgrade");
 
         let key = format!(
             "{}{}",
             &headers[SEC_WEBSOCKET_KEY].to_str().unwrap(),
             Self::WS_KEY
         );
+
+        let len_a = &headers[SEC_WEBSOCKET_KEY].len();
+        let len_b = Self::WS_KEY.len();
+
+        let mut bytes = Vec::with_capacity(len_a + len_b);
+        bytes.extend_from_slice(headers[SEC_WEBSOCKET_KEY].as_bytes());
+        bytes.extend_from_slice(Self::WS_KEY.as_bytes());
+
         let hashed = Sha1::default().digest(key.as_bytes()).to_bytes();
         let encoded = encode(hashed);
         builder = builder.header(SEC_WEBSOCKET_ACCEPT, encoded);
@@ -40,8 +59,7 @@ impl WsUpgrader {
             .body(())
             .unwrap();
 
-        let mut buf = BytesMut::new();
-
+        let mut http: Http<()> = Http::new();
         http.encode(resp, &mut buf).unwrap();
         stream.write(&buf).await?;
 
